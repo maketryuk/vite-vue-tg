eac249c0fbc65092d06ddf15730da56181868188
maketryuk <langstons.pr@gmail.com>
2024-02-12 16:14:01 +0300

    fixes

--- src/init.ts
+++ src/init.ts
@@ -1,11 +1,10 @@
 import { initService } from "./services/serviceTelegram.ts";
 
 export default async (config: any) => {
-	console.log(config);
 	try {
 		const queryId = await initService();
 
-		console.log(queryId);
+		return { queryId: config.VITE_TELEGRAM_AUTHORIZATION || queryId };
 	} catch (error) {
 		console.log(error);
 	}
